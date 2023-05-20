9f1b257ca5be419b688712f8a995f715498ae2c8
Marcosl21 <23256535-Marcosl21@users.noreply.replit.com>
2023-05-20 13:50:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let salario_bruto= 6500;
+let ir = salario_bruto * 0.10;
+let salario_liquido = salario_bruto - ir;
+console.log("salario recebido: R$" + salario_liquido);
---
+++ readme.md
@@ -0,0 +1,1 @@
+Problema com imposto de Renda.
