49a2231f1becba029a7bb29eaa359fed5a66d05a
Marcosl21 <23256535-Marcosl21@users.noreply.replit.com>
2023-05-20 13:55:01 +0000

    operadores

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let salario_bruto= 6500;
+let salario_bruto = 5500;
 let ir = salario_bruto * 0.10;
 let salario_liquido = salario_bruto - ir;
 console.log("salario recebido: R$" + salario_liquido);
